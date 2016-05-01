6a988b0128817cc38c96b8d7626088c4929eb3b9
Benjamin Bein <benjamin.bein@web.de>
2016-05-01 16:18:45 -0400

    added a commen header file for future analysis scripts

---
+++ analysis/header.py
@@ -0,0 +1,13 @@
+'''
+common header file for the analysis scripts
+'''
+
+import sqlite3 as lite
+import sys
+import numpy as np
+from dateutil.parser import parse
+import time
+import datetime
+import csv
+import matplotlib.pyplot as plt #plotting
+import math as m
