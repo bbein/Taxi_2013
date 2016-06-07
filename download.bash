20d742acd8fab0f88701dfdd261251e82494c4c4
Benjamin Bein <benjamin.bein@web.de>
2016-06-07 14:57:32 -0400

    updated the download file so that it also downloads the fare data for 2013

--- download.bash
+++ download.bash
@@ -1,9 +1,18 @@
 #!/bin/bash
 source1="https://storage.googleapis.com/tlc-trip-data/"
+source2="https://nyctaxitrips.blob.core.windows.net/data/trip_fare_"
 target="./raw_data/"
 
 #make directory i it doesn't exists
 mkdir -p -- "$target"
+#download fare data for 2013
+for month in {1..12}
+do
+	wget -O "${target}trip_fare_${month}.csv.zip" "${source2}${month}.csv.zip"
+	unzip "${target}trip_fare_${month}.csv.zip" -d "${target}"
+	rm "${target}trip_fare_${month}.csv.zip"
+done
+#download trip data for yellow and green cabs
 for year in {2009..2015}
 do
 	for month in {01..12} 
@@ -15,5 +24,5 @@ do
 		fi
 	done
 done
-
-
+ 
+	
